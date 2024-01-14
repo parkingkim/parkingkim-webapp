64397a541503e37af1f13fe08356f69f94ccbc2c
강영민 <kym6050@hanyang.ac.kr>
2024-01-14 16:39:16 +0900

    feat: 공통 색상 변수 관리를 위한 Themeprovider 적용 (#4)
    * style: style 정렬 속성 추가
    
    * feat: 공통 색상 변수 theme 선언과 적용
    
    * feat: favicon 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { ThemeProvider } from 'styled-components';
 import './App.css';
 import Button from '@components/Button';
+import theme from './theme.ts';
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Button />
-    </div>
+    </ThemeProvider>
   );
 }
 
