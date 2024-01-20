58004602816e1ef75146d26c71ef1e726c96c8c3
regularPark <srk99279@naver.com>
2024-01-20 17:38:26 +0900

    design: 피그마에 맞게 스타일 수정

--- src/components/BottomSheet/index.tsx
+++ src/components/BottomSheet/index.tsx
@@ -8,33 +8,41 @@ const BottomSheetContainer = styled.div<{ height: number }>`
   bottom: 0;
   left: 0;
   right: 0;
-  background-color: red;
+  background-color: #fff;
   border-radius: 10px 10px 0 0;
+  box-shadow: 0px 4px 5px 6px rgba(0, 0, 0, 0.25);
   height: ${({ height }) => height}px;
   transition: height 0.3s ease;
   touch-action: none; // iOS Safari의 스크롤 방지
 `;
 
-const DragHandle = styled.span`
+const DragHandle = styled.div`
   width: 100%;
-  padding: 10px;
+  height: 40px;
   text-align: center;
-  background-color: #ddd;
   cursor: pointer;
+`;
+
+const DragHandleBar = styled.div`
+  width: 85px;
+  height: 3px;
+  margin: 12px auto;
+  border-radius: 4px;
+  background-color: #000;
 `;
 
 const BottomSheet = ({ children }: PropsWithChildren) => {
   const { height, handleDragStart, handleDragMove, handleDragEnd } = useDraggable(INITIAL_HEIGHT);
 
-  const handleMouseDown = (e: MouseEvent<HTMLSpanElement>) => {
+  const handleMouseDown = (e: MouseEvent<HTMLDivElement>) => {
     handleDragStart(e.clientY);
   };
 
-  const handleTouchStart = (e: TouchEvent<HTMLSpanElement>) => {
+  const handleTouchStart = (e: TouchEvent<HTMLDivElement>) => {
     handleDragStart(e.touches[0].clientY);
   };
 
-  const handleTouchMove = (e: TouchEvent<HTMLSpanElement>) => {
+  const handleTouchMove = (e: TouchEvent<HTMLDivElement>) => {
     handleDragMove(e.touches[0].clientY);
   };
 
@@ -50,7 +58,7 @@ const BottomSheet = ({ children }: PropsWithChildren) => {
         onTouchMove={handleTouchMove}
         onTouchEnd={handleTouchEnd}
       >
-        드래그 바
+        <DragHandleBar />
       </DragHandle>
       {children}
     </BottomSheetContainer>
