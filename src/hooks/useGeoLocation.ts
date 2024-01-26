cd4d17eb24cf1151b51c3df0ee3596994e1967fe
regularPark <srk99279@naver.com>
2024-01-26 18:44:52 +0900

    feat: tmap api 현위치 이동 위한 `useGeoLocation`훅 생성

---
+++ src/hooks/useGeoLocation.ts
@@ -0,0 +1,46 @@
+import { useRef, useState, useEffect } from 'react';
+
+type GeoLocation = {
+  lat: number;
+  lng: number;
+};
+
+const useGeoLocation = (mapInstance: any) => {
+  const { Tmapv3 } = window;
+  const [location, setLocation] = useState<GeoLocation | null>(null);
+  const tmapMarker = useRef<any>(null);
+
+  useEffect(() => {
+    if (location && mapInstance.current) {
+      const userCoords = new Tmapv3.LatLng(location.lat, location.lng);
+
+      mapInstance.current.setCenter(userCoords);
+      mapInstance.current.setZoom(15);
+
+      if (tmapMarker.current) {
+        tmapMarker.current.setMap(null); // 기존 마커 제거
+      }
+
+      tmapMarker.current = new Tmapv3.Marker({
+        position: userCoords,
+        icon: 'src/assets/location.svg',
+        map: mapInstance.current,
+      });
+    }
+  }, [location, mapInstance, Tmapv3]);
+
+  const makeUserMaker = () => {
+    if (!mapInstance.current) return;
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLocation({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+    });
+  };
+
+  return { makeUserMaker };
+};
+
+export default useGeoLocation;
