43c2cab2cf8cee5076d5117105b907a27186f6c6
박정규 <srk99279@naver.com>
2024-01-29 21:50:16 +0900

    feat: `BottomSheet`과 `SearchBar` 합치기 (#13)
    * refactor: 스타일 코드 하단으로 이동
    
    * feat: 뒤로가기 아이콘 추가
    
    * feat: 라우팅 구현
    
    * refactor: 서치바 내부 로직 변경
    
    * design: `MobileView` 높이 수정
    
    * feat: svg 추가
    
    * feat: 조건설정 및 현위치 버튼 추가
    
    * design: 피그마에 맞춰 스타일 변경
    
    * feat: `SearchFilter` 버튼 구현
    
    * design: 스타일과 뷰포트 수정
    
    * refactor: 피드백 반영
    
    드래그바 제거, 기본 높이 증가 등
    
    * style: 주석 제거
    
    * style: `SearchFilter` 디렉토리 변경
    
    * style: `SearchFilter` 디렉토리 변경
    
    * feat: 공통되는 스타일 `commonTextStyle`로 분리
    
    * refactor: 공통 스타일 적용 및 lint 적용

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,63 @@
 import Map from '@components/Map';
-import MobileView from '@components/MobileView';
+import BottomSheet from '@components/BottomSheet';
+import SearchBar from '@components/SearchBar';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import SearchFilter from '@pages/Home/components/SearchFilter';
+import { CommonTextStyle } from '@style/CommonTextStyle';
 
 const Home = () => {
+  const navigate = useNavigate();
+  const goSearch = () => {
+    navigate('/search');
+  };
+
   return (
-    <MobileView>
+    <>
       <Map />
-    </MobileView>
+      <BottomSheet>
+        <SearchBarWrapper>
+          <Title>목적지 입력</Title>
+          <SearchBar isFocused={false} goSearch={goSearch} />
+          <SearchFilter />
+        </SearchBarWrapper>
+      </BottomSheet>
+    </>
   );
 };
+
+const SearchBarWrapper = styled.div`
+  display: flex;
+  padding: 22px 20px;
+  flex-direction: column;
+  align-items: flex-start;
+
+  border-bottom: 7px solid #f6f6f6;
+
+  color: #120924;
+  font-size: 20px;
+  font-weight: 700;
+  ${CommonTextStyle}
+
+  gap: 15px;
+
+  & > :first-child {
+    margin-left: 2px;
+  }
+
+  & > :last-child {
+    margin-left: 8px;
+  }
+`;
+
+const Title = styled.h2`
+  padding-left: 4px;
+  margin: 0;
+
+  color: #120924;
+  font-size: 20px;
+  font-weight: 700;
+  ${CommonTextStyle}
+`;
 
 export default Home;
