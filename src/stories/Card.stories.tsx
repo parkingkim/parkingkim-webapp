08cd127a3621f915020d6009e2a58f5d13adf1b0
regularPark <srk99279@naver.com>
2024-01-30 13:25:08 +0900

    test: `Card` 스토리북 작성

---
+++ src/stories/Card.stories.tsx
@@ -0,0 +1,40 @@
+import Card from '@components/Card';
+
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Components/Card',
+  component: Card,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof Card>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    title: '부산대학교 주차장',
+    price: 4500,
+    ETA: 15,
+    isPublic: true,
+    isFavorite: false,
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            padding: '10px',
+          }}
+        >
+          <Story />
+        </div>
+      );
+    },
+  ],
+};
