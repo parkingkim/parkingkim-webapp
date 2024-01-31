4f976eb094cdda1d8a32a09d5c6a00ec1fdc07c1
regularPark <srk99279@naver.com>
2024-01-31 13:37:17 +0900

    test: `Text` 스토리북 작성

---
+++ src/stories/Text.stories.tsx
@@ -0,0 +1,55 @@
+import Text from '@components/Text';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Components/Text',
+  component: Text,
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    children: 'Text',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Text>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+export const ExtraBold: Story = {
+  args: {
+    size: 'x-bold',
+    children: 'Pretendard,ExtraBold,24pt,-3%',
+  },
+};
+export const SemiBold: Story = {
+  args: {
+    size: 'semi-bold',
+    children: 'Pretendard,SemiBold,22pt,-2%',
+  },
+};
+export const Bold: Story = {
+  args: {
+    size: 'bold',
+    children: 'Pretendard, Bold,20pt',
+  },
+};
+export const Medium: Story = {
+  args: {
+    size: 'md',
+    children: 'Pretendard,Medium,16',
+  },
+};
+export const Regular: Story = {
+  args: {
+    size: 'regular',
+    children: 'Pretendard,Regular,14pt,-3%',
+  },
+};
+export const SmallRegular: Story = {
+  args: {
+    size: 'sm-regular',
+    children: 'Pretendard, Regular, 11pt, -3%',
+  },
+};
