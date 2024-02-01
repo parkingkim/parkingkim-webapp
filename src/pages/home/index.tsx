8458ac2c322771b0b0ac48b037067e909ead3851
regularPark <srk99279@naver.com>
2024-02-01 17:38:24 +0900

    refactor: `Home` 컴포넌트 수정

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,63 +1,24 @@
 import Map from '@components/Map';
 import BottomSheet from '@components/BottomSheet';
-import SearchBar from '@components/SearchBar';
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import SearchFilter from '@pages/Home/components/SearchFilter';
-import { CommonTextStyle } from '@style/CommonTextStyle';
+import { useState } from 'react';
+import SearchContent from './components/SearchContent';
+import InitialContent from './components/InitialContent';
 
 const Home = () => {
-  const navigate = useNavigate();
-  const goSearch = () => {
-    navigate('/search');
-  };
+  const [isExpanded, setIsExpanded] = useState(false);
 
   return (
     <>
       <Map />
-      <BottomSheet>
-        <SearchBarWrapper>
-          <Title>목적지 입력</Title>
-          <SearchBar isFocused={false} goSearch={goSearch} />
-          <SearchFilter />
-        </SearchBarWrapper>
+      <BottomSheet isExpanded={isExpanded}>
+        {isExpanded ? (
+          <SearchContent reduceHeight={() => setIsExpanded(false)} />
+        ) : (
+          <InitialContent setExpanded={setIsExpanded} />
+        )}
       </BottomSheet>
     </>
   );
 };
-
-const SearchBarWrapper = styled.div`
-  display: flex;
-  padding: 22px 20px;
-  flex-direction: column;
-  align-items: flex-start;
-
-  border-bottom: 7px solid #f6f6f6;
-
-  color: #120924;
-  font-size: 20px;
-  font-weight: 700;
-  ${CommonTextStyle}
-
-  gap: 15px;
-
-  & > :first-child {
-    margin-left: 2px;
-  }
-
-  & > :last-child {
-    margin-left: 8px;
-  }
-`;
-
-const Title = styled.h2`
-  padding-left: 4px;
-  margin: 0;
-
-  color: #120924;
-  font-size: 20px;
-  font-weight: 700;
-  ${CommonTextStyle}
-`;
 
 export default Home;
