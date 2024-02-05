719fe69344174e94817b56dad205d185def95f65
regularPark <srk99279@naver.com>
2024-02-05 12:39:48 +0900

    test: `Coupon` 스토리북 작성

---
+++ src/stories/Coupon.stories.tsx
@@ -0,0 +1,37 @@
+import Coupon from '@pages/Menu/components/Coupon';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Components/Coupon',
+  component: Coupon,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof Coupon>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    discountRate: 10,
+    discountName: '설날 연휴 공용주차장',
+    expirationDate: '2022-02-28',
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            padding: '100px',
+          }}
+        >
+          <Story />
+        </div>
+      );
+    },
+  ],
+};
