c47bb3b8010bd0cdacc5b44eb579d68f710dd413
regularPark <srk99279@naver.com>
2024-02-24 03:17:02 +0900

    test: `FavParkingLotsItem` 스토리북 작성

---
+++ src/stories/FavParkingLotItem.stories.tsx
@@ -0,0 +1,36 @@
+import FavParkingLotItem from '@pages/MyParkingLots/components/FavParkingLotItem';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Components/FavoriteParkingLotItem',
+  component: FavParkingLotItem,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof FavParkingLotItem>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    parkingLot: {
+      parkingId: '1',
+      parkingName: '영호주차장',
+      estimatedFee: '3000',
+      estimatedWalkingTime: '',
+      parkingType: '노외',
+      isFavorite: true,
+      latitude: '34.333213',
+      longitude: '54.434312',
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ width: '300px', height: '100px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
