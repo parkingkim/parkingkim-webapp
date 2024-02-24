f8c06e7e92fedab7dc14fa90673ab376d64e4a21
kangyeongmin <kym6050@hanyang.ac.kr>
2024-02-24 18:05:41 +0900

    refactor: BottomTabBar 유무 따른 router 분리

---
+++ src/router.tsx
@@ -0,0 +1,59 @@
+import { Outlet, createBrowserRouter } from 'react-router-dom';
+import BottomTabBar from '@components/BottomTabBar';
+import MobileView from '@components/MobileView';
+import Home from '@pages/Home';
+import Login from '@pages/Login';
+import Menu from '@pages/Menu';
+import Coupons from '@pages/Menu/Coupons';
+import FrequentDestinations from '@pages/Menu/FrequentDestinations';
+import Notifications from '@pages/Menu/Notifications';
+import Profile from '@pages/Menu/Profile';
+import ChangePassword from '@pages/Menu/Profile/ChangePassword';
+import OnBoarding from '@pages/OnBoarding';
+import ParkingLotDetail from '@pages/ParkingLotDetail';
+import Search from '@pages/Search';
+
+const Root = () => {
+  return (
+    <MobileView>
+      <Outlet />
+    </MobileView>
+  );
+};
+
+const BottomTabBarRoot = () => {
+  return (
+    <MobileView>
+      <Outlet />
+      <BottomTabBar />
+    </MobileView>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      { path: '/login', element: <Login /> },
+      { path: '/onboarding', element: <OnBoarding /> },
+      { path: '/menu', element: <Menu /> },
+      { path: '/profile', element: <Profile /> },
+      { path: '/coupons', element: <Coupons /> },
+      { path: '/notifications', element: <Notifications /> },
+      { path: '/change-password', element: <ChangePassword /> },
+      { path: '/frequent-destinations', element: <FrequentDestinations /> },
+    ],
+  },
+  {
+    path: '/',
+    element: <BottomTabBarRoot />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/search', element: <Search /> },
+      { path: '/parking-detail', element: <ParkingLotDetail /> },
+    ],
+  },
+]);
+
+export default router;
