cfebbf2ce988376903605dd4c01a2c07b0110c35
regularPark <srk99279@naver.com>
2024-02-25 22:14:32 +0900

    feat: 탈퇴하기 페이지 구현

---
+++ src/pages/Menu/Withdrawal/components/ConfirmWithdrawal.tsx
@@ -0,0 +1,152 @@
+import { BackIcon, CheckIcon, CheckedRadioIcon, RadioIcon } from '@assets/index';
+import Button from '@components/Button';
+import Text from '@components/Text';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+interface ConfirmWithdrawalProps {
+  cancelWithdrawal: () => void;
+}
+
+const options: string[] = [
+  '원하는 기능이나 서비스가 없어서',
+  '서비스가 만족스럽지 않아서',
+  '당분간 사용할 계획이 없어서',
+  '직접 입력',
+];
+
+const ConfirmWithdrawal = ({ cancelWithdrawal }: ConfirmWithdrawalProps) => {
+  const [selectedOption, setSelectedOption] = useState('');
+  const [customInput, setCustomInput] = useState('');
+
+  const handleOptionChange = (option: string) => {
+    setSelectedOption(option);
+    if (option !== '직접 입력') {
+      setCustomInput('');
+    }
+  };
+
+  return (
+    <>
+      <BackIcon onClick={cancelWithdrawal} style={{ minHeight: '42px' }} />
+      <Header>
+        <Text size="3xl" fontStyle="bold">
+          파킹킴과 함께해주셔서 감사했어요!
+        </Text>
+        <Text color="red" fontStyle="semi-bold">
+          탈퇴 시 주의사항
+        </Text>
+        <Description>
+          <CheckIcon />
+          <Text>파킹킴의 조건설정을 통한 목적지까지의 경로는 더 이상 사용할 수 없습니다.</Text>
+        </Description>
+        <Description>
+          <CheckIcon />
+          <Text>쿠폰, 즐겨찾기 등의 이용내역은 즉시 소멸됩니다.</Text>
+        </Description>
+      </Header>
+      <OptionContainer>
+        <Text size="lg" fontStyle="semi-bold">
+          더 나은 파킹킴이 되기 위해, 탈퇴 사유를 말씀해 주세요.
+        </Text>
+        <OptionWrapper>
+          {options.map((option, index) => (
+            <Option key={index} onClick={() => handleOptionChange(option)}>
+              {selectedOption === option ? <CheckedRadioIcon /> : <RadioIcon />}
+              {option !== '직접 입력' ? (
+                <Text>{option}</Text>
+              ) : (
+                <StyledInput
+                  placeholder="이유를 입력해주세요"
+                  value={customInput}
+                  $isCustomInput={customInput !== ''}
+                  onChange={(e) => setCustomInput(e.target.value)}
+                />
+              )}
+            </Option>
+          ))}
+        </OptionWrapper>
+        <Button size="large" color="secondary">
+          <Text fontStyle="bold" color="white" size="lg">
+            탈퇴하기
+          </Text>
+        </Button>
+      </OptionContainer>
+    </>
+  );
+};
+
+const Header = styled.header`
+  display: flex;
+  padding-bottom: 45px;
+  flex-direction: column;
+  align-items: flex-start;
+
+  border-bottom: 2px solid #f6f6f6;
+  gap: 12px;
+
+  & > :first-child {
+    width: 230px;
+    margin-bottom: 50px;
+    text-align: left;
+  }
+`;
+
+const Description = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 15px;
+
+  svg {
+    width: 12px;
+    min-width: 12px;
+  }
+`;
+
+const OptionContainer = styled.div`
+  display: flex;
+  margin-bottom: 80px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 24px;
+
+  & > :first-child {
+    text-align: left;
+    margin-top: 25px;
+  }
+
+  & > :last-child {
+    margin-top: 70px;
+  }
+`;
+
+const OptionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+const Option = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+  cursor: pointer;
+`;
+
+const StyledInput = styled.input<{ $isCustomInput: boolean }>`
+  width: 300px;
+  padding-bottom: 4px;
+
+  background-color: transparent;
+  border: none;
+  border-radius: 0;
+  border-bottom: 1px solid
+    ${({ theme, $isCustomInput }) => ($isCustomInput ? theme.darkGray : theme.gray)};
+
+  font-size: 16px;
+
+  transition: all 0.2s ease-in-out;
+`;
+
+export default ConfirmWithdrawal;
---
+++ src/pages/Menu/Withdrawal/components/SelectWithdrawal.tsx
@@ -0,0 +1,137 @@
+import { BackIcon, CheckIcon } from '@assets/index';
+import Button from '@components/Button';
+import Text from '@components/Text';
+import { Dispatch, SetStateAction } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+interface SelectWithdrawalProps {
+  setIsWithdrawal: Dispatch<SetStateAction<boolean>>;
+}
+
+const SelectWithdrawal = ({ setIsWithdrawal }: SelectWithdrawalProps) => {
+  const navigate = useNavigate();
+
+  const goToMenu = () => {
+    navigate('/menu');
+  };
+
+  const selectWithdrawal = () => {
+    setIsWithdrawal(true);
+  };
+
+  return (
+    <>
+      <Header>
+        <BackIcon onClick={goToMenu} />
+        <Image />
+        <Text fontStyle="bold" size="3xl">
+          정말로 파킹킴을 떠나시나요?
+        </Text>
+      </Header>
+      <ContentContainer>
+        <DescriptionContainer>
+          <Text fontStyle="semi-bold" size="lg">
+            계정을 유지하면 이런 혜택이 남아있어요!
+          </Text>
+          <Description>
+            <CheckIcon />
+            <Text>원하는 조건대로 편리하게 목적지까지 갈 수 있어요.</Text>
+          </Description>
+          <Description>
+            <CheckIcon />
+            <Text>다양한 이벤트와 혜택으로 저렴하게 주차장을 사용할 수 있어요.</Text>
+          </Description>
+        </DescriptionContainer>
+        <CautionContainer>
+          <Text fontStyle="semi-bold" size="lg">
+            그래도 떠나시겠어요?
+          </Text>
+          <Text color="red">
+            탈퇴 시 현재 보유중인 쿠폰, 즐겨찾기 등은 즉시 소멸되어 복구할 수 없습니다.
+          </Text>
+        </CautionContainer>
+      </ContentContainer>
+      <ButtonContainer>
+        <Button size="large" color="secondary" onClick={goToMenu}>
+          <Text color="white" size="xl" fontStyle="bold">
+            아니요, 더 사용할게요.
+          </Text>
+        </Button>
+        <Button size="large" color="primary" onClick={selectWithdrawal}>
+          <Text size="xl" fontStyle="bold">
+            네, 탈퇴할래요.
+          </Text>
+        </Button>
+      </ButtonContainer>
+    </>
+  );
+};
+
+const Header = styled.header`
+  display: flex;
+  padding-bottom: 55px;
+  flex-direction: column;
+
+  border-bottom: 2px solid #f6f6f6;
+  gap: 30px;
+
+  & > :nth-child(2) {
+    margin: 0 auto;
+  }
+`;
+
+const Image = styled.div`
+  width: 100px;
+  height: 100px;
+
+  background-color: ${({ theme }) => theme.gray};
+  border-radius: 50%;
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  padding: 30px 0;
+  flex-direction: column;
+  align-items: flex-start;
+  text-align: left;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  width: 100%;
+  margin-bottom: 80px;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+`;
+
+const DescriptionContainer = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  flex-direction: column;
+
+  & > p {
+    margin-bottom: 20px;
+  }
+`;
+
+const Description = styled.div`
+  display: flex;
+  margin-bottom: 8px;
+  align-items: center;
+  gap: 15px;
+
+  svg {
+    width: 12px;
+    min-width: 12px;
+  }
+`;
+
+const CautionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export default SelectWithdrawal;
