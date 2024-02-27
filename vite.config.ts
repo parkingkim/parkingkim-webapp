e00740bb6517f2c7bec8ae86dd0f3c6ac895268f
regularPark <srk99279@naver.com>
2024-02-27 17:00:23 +0900

    chore: zustand 설치

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       { find: '@utils', replacement: '/src/utils' },
       { find: '@constants', replacement: '/src/constants' },
       { find: '@style', replacement: '/src/style' },
+      { find: '@store', replacement: '/src/store' },
     ],
   },
   build: {
