760a3ef200cce005c7898ff2939980cc551dec1d
regularPark <srk99279@naver.com>
2024-04-29 18:46:32 +0900

    feat: 목적지 마커 추가

--- src/store/userMarkerStore.ts
+++ src/store/userMarkerStore.ts
@@ -1,18 +1,22 @@
 import { GeoLocation } from 'src/types/map';
 import { create } from 'zustand';
 
-interface UserMarkerStore {
+interface MarkerStore {
   userMarker: Marker | null;
   userLocation: GeoLocation | null;
+  destinationMarker: Marker | null;
   setUserLocation: (userLocation: GeoLocation | null) => void;
   setUserMarker: (userMarker: Marker | null) => void;
+  setDestinationMarker: (destinationMarker: Marker | null) => void;
 }
 
-const useUserMarkerStore = create<UserMarkerStore>((set) => ({
+const useMarkerStore = create<MarkerStore>((set) => ({
   userMarker: null,
   userLocation: null,
+  destinationMarker: null,
   setUserMarker: (userMarker) => set({ userMarker }),
   setUserLocation: (userLocation) => set({ userLocation }),
+  setDestinationMarker: (destinationMarker) => set({ destinationMarker }),
 }));
 
-export default useUserMarkerStore;
+export default useMarkerStore;
