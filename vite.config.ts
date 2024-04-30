d81a1dbdae2400a3094cff25e7d7505e065609bf
regularPark <srk99279@naver.com>
2024-04-30 12:05:05 +0900

    chore: 컨텍스트 경로 추가

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
       { find: '@style', replacement: '/src/style' },
       { find: '@store', replacement: '/src/store' },
       { find: '@apis', replacement: '/src/apis' },
+      { find: '@context', replacement: '/src/context' },
     ],
   },
   build: {
