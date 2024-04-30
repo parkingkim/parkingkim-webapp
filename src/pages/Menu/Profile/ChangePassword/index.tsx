584cda203acd4ef031283ec6cd1a81acbab95b12
regularPark <srk99279@naver.com>
2024-04-30 19:50:03 +0900

    feat: 유효성 검사 추가 및 디자인 변경

--- src/pages/Menu/Profile/ChangePassword/index.tsx
+++ src/pages/Menu/Profile/ChangePassword/index.tsx
@@ -2,12 +2,24 @@ import { BackIcon } from '@assets/index';
 import { HeadContainer } from '..';
 import { useNavigate } from 'react-router-dom';
 import Text from '@components/Text';
-import { Partition } from '../..';
 import PasswordInput from './components/PasswordInput';
 import styled from 'styled-components';
+import usePassword from '../hook/usePassword';
 
 const ChangePassword = () => {
   const navigate = useNavigate();
+  const {
+    curPassword,
+    newPassword,
+    againPassword,
+    validateCurPassword,
+    validateNewPassword,
+    validateAgainPassword,
+    isCurPasswordValid,
+    isNewPasswordValid,
+    isAgainPasswordValid,
+  } = usePassword();
+
   const goBack = () => {
     navigate('/profile');
   };
@@ -23,24 +35,58 @@ const ChangePassword = () => {
           파킹킴 아이디의 비밀번호를 변경해주세요.
         </Text>
       </HeadContainer>
-      <Partition />
+      <ThickBar />
       <InputContainer>
         <CurrentPasswordWrapper>
-          <Text size="sm">현재 비밀번호</Text>
-          <PasswordInput placeholder="현재 비밀번호 입력" />
-          <a>현재 비밀번호를 잊으셨나요?</a>
+          <Text size="sm" color="gray80">
+            현재 비밀번호
+          </Text>
+          <PasswordInput placeholder="현재 비밀번호 입력" onChange={validateCurPassword} />
+          <Text size="xs" color="error">
+            현재 비밀번호를 잊으셨나요?
+          </Text>
         </CurrentPasswordWrapper>
         <NewPasswordContainer>
-          <Text size="sm">새 비밀번호</Text>
-          <PasswordInput placeholder="영문, 숫자, 특수문자 포함 8자 이상" />
-          <Text size="sm">새 비밀번호 확인</Text>
-          <PasswordInput />
+          <Text size="sm" color="gray80">
+            새 비밀번호
+          </Text>
+          <PasswordInput
+            placeholder="영문, 숫자, 특수문자 포함 8자 이상"
+            onChange={validateNewPassword}
+          />
+          <Text size="sm" color="gray80">
+            새 비밀번호 확인
+          </Text>
+          <PasswordInput
+            placeholder="새 비밀번호를 재입력해주세요"
+            onChange={validateAgainPassword}
+          />
         </NewPasswordContainer>
-        <ChangePasswordButton>완료</ChangePasswordButton>
+        <ChangePasswordButton
+          disabled={
+            !isCurPasswordValid ||
+            !isNewPasswordValid ||
+            !isAgainPasswordValid ||
+            curPassword !== newPassword ||
+            newPassword !== againPassword
+          }
+        >
+          <Text size="xl" color="white" fontStyle="bold">
+            저장하기
+          </Text>
+        </ChangePasswordButton>
       </InputContainer>
     </>
   );
 };
+
+const ThickBar = styled.div`
+  width: 100%;
+  height: 7px;
+  min-width: 100%;
+
+  background: linear-gradient(to bottom, #dcdcdc 0%, #f6f6f6 30%);
+`;
 
 const InputContainer = styled.div`
   display: flex;
@@ -90,15 +136,21 @@ const ChangePasswordButton = styled.button`
   justify-content: center;
   align-items: center;
 
-  background-color: ${({ theme }) => theme.gray};
+  background-color: ${({ theme }) => theme.blue100};
   border-radius: 10px;
 
   position: absolute;
-  bottom: 0;
+  bottom: 20px;
 
   color: white;
   font-size: 20px;
   cursor: pointer;
+  box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.15);
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.gray60};
+    cursor: not-allowed;
+  }
 `;
 
 export default ChangePassword;
